404b1550436b452420c7e005a0791c79d692adb2
rayraycodes <imregan@umich.edu>
2024-04-14 19:49:57 -0400

    Prepare Next.js app for deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-// This is a JS module that exports the configuration object for Next.js
-// The configuration includes type support for better IntelliSense in supported editors.
+/** @type {import('next').NextConfig} */
+const nextConfig = {output: 'standalone'};
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    // React strict mode helps to identify potential problems in an application.
-    reactStrictMode: true,
-  
-  
-    // Static export configuration
-    output: 'standalone',  // This enables support for static HTML export in Next.js
-  
-    // Additional configurations can be added as needed
-  };
-  
-  module.exports = nextConfig;
-  
+export default nextConfig;
