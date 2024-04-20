a50ef9bd733f653e04cb1541aab2abc5e8933662
rayraycodes <imregan@umich.edu>
2024-04-20 18:35:51 -0400

    added more components and breadcrumb design with table css

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -10,7 +10,7 @@ interface TagsProps {
 
 export const Tags: React.FC<TagsProps> = ({ pages }) => {
   return (
-    <div className="flex flex-wrap animate-slide-fade-in">
+    <div className="flex flex-wrap justify-center animate-slide-fade-in">
       {pages.map((page) => (
         <Link href={page.path} key={page.path}>
           <button className="m-1 px-2 py-1 bg-gray-500 text-white rounded opacity-50 hover:opacity-100">{page.label}</button>
